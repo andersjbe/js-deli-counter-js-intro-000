7aacbc0df2c041797bb14e5867bc48d3748ca629
Ben Anderson <andersjbe@gmail.com>
2020-03-22 23:13:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber(line, newPerson) {
   line.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line) {
+  if(line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  return line.dequeue();
+}
