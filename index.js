cd5db9190119fc8cdde2606d1826b9f7713e98f6
Ben Anderson <andersjbe@gmail.com>
2020-03-22 23:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, newPerson) {
   line.push(newPerson);
-  return `Welcome ${newPerson}, your are #${line.length} in line!`;
+  return `Welcome ${newPerson}, your are number ${line.length} in line!`;
 }
